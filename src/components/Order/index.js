7edadff5c35c45f27b152818dfc5a3541a5baf29
Valerie Kalchenko <valerie.kalchenko@gmail.com>
2023-04-19 10:45:19 -0400

    date added to order

--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Image, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const Order = ({order}) => {
 
   
-
+  const [createdAt, setCreatedAt] = useState([]);
   const navigation = useNavigation();
+
+  useEffect(() => {
+    if (!order.createdAt) {
+      return;
+    }
+
+    const results = order?.createdAt?.split('T');
+    setCreatedAt(results);
+
+  }, [order.createdAt]);
   
 
   const onPress = () => {
@@ -24,7 +34,7 @@ const Order = ({order}) => {
             <Text style={{fontWeight: '600', fontSize: 16,}}>{order.Restaurant.name}</Text>
             <Text style={{color: 'grey',}}>Total: ${order.total.toFixed(2)}</Text>
             <Text style={{color: 'grey',}}>Status: {order.status}</Text>
-            <Text style={{color: 'grey',}}>Pickup Time: {order.pickUpTime}</Text>
+            <Text style={{color: 'grey',}}>Pickup Date/Time: {order.createdAt ? createdAt[0] : ''} {order.pickUpTime}</Text>
         </View>  
     </Pressable>
   );
